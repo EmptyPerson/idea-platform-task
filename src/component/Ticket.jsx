a9cdca3ab5032939b6d734269a944bb4955250e1
Булахов Владислав <vladislav.bulahov@yandex.ru>
2023-09-13 18:56:48 +0300

    first commit

---
+++ src/component/Ticket.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ticket = ({ ticket }) => {
+    return (
+        <div className="ticket">
+            {/* Отобразите информацию о билете, например, цену, маршрут, и т. д. */}
+        </div>
+    );
+};
+
+export default Ticket;
